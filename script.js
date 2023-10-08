de6f4f26146e6df6d23d43d84ccdc00930c725c2
osechi-1111 <121627718+osechi-1111@users.noreply.github.com>
2023-10-08 14:01:48 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log("unko");
+// 今日の日付
+let today = new Date();
+let month = today.getMonth() + 1;
+let date = today.getDate();
+const weekDay = ["日","月","火","水","木","金","土"];
+let day = weekDay[today.getDay()];
+const dateBox = document.getElementById("dateBox");
+
+dateBox.innerText = month + "/" + date + " " + "(" + day + ")";
